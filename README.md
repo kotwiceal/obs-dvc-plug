a2c8e25e41bfcdd30c16987bd32e3d4c681fc8cb
kotwiceal <kotvitskyaya@gmail.com>
2025-08-04 23:42:12 +0300

    project release, implemented add/push/pull dvc commands, append readme and license

--- README.md
+++ README.md
@@ -1,94 +1,61 @@
-# Obsidian Sample Plugin
+## Obsidian DVC (Data Version Control) Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+Manage and version data by [`dvc`](https://dvc.org/) toolkit in the Obsidian environment.
+Plugin is inspired to improve collaboration work by management large files 
+that are not necessary to track and/or cache by git directly.
+It is applicable to log the laboratory routine, data processing result etc.
 
-This project uses TypeScript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in TypeScript Definition format, which contains TSDoc comments describing what it does.
+#### Functionality
+Following features are implemented:
+- file contex menu of `dvc` commands
+- auto pulling markdown attachments by specified file extensions
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+#### Requirements
+1. It's necessary have pre installed softwares:
+    - git
+    - python
+2. Also installed [`dvc`](https://dvc.org/doc/install) package:
 
-## First time developing plugins?
+    ```bash
+    pip install dvc
+    ```
 
-Quick starting guide for new plugin devs:
+#### Preparing
+- initialize [`git`](https://git-scm.com/docs/git-init) and [`dvc`](https://dvc.org/doc/command-reference/init) inside Obsidian vault directory by following commands
+    
+    ```bash
+    git init & dvc init
+    ```
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+    or enable the plugin and execute Obsidian command `dvc: init`
 
-## Releasing new releases
+- create [remote](https://dvc.org/doc/command-reference/remote) `storage` (for example) by following command
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+    ```bash
+    dvc remote add -d storage <storage-path>
+    ```
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+#### Restriction
+This plugin is only compatible with desktop devices.
 
-## Adding your plugin to the community plugin list
+## Examples
 
-- Check the [plugin guidelines](https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines).
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+- Append to stage a single file:
 
-## How to use
+    ![](media/dvc-add-file.gif)
 
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
+- Append to stage multiple files:
 
-## Manually installing the plugin
+    ![](media/dvc-add-files.gif)
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+- Push to remote multiple files:
 
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
+    ![](media/dvc-push-files.gif)
 
-## Funding URL
+- Pull from remote multiple files:
 
-You can include funding URLs where people who use your plugin can financially support it.
+    ![](media/dvc-pull-files.gif)
 
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
+- Enable auto markdown attachments pulling:
 
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+    ![](media/dvc-auto-pull.gif)
--- main.ts
+++ main.ts
@@ -1,81 +1,203 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, 
+	Setting, TAbstractFile, TFile, FileSystemAdapter } from 'obsidian';
+import { exec } from 'child_process';
+import { isArray } from 'util';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface DVCPluginSettings {
+	autostage: boolean;
+	autopull: boolean;
+	autopullExtension: string[];
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DVCPluginSettings = {
+	autostage: false,
+	autopull: false,
+	autopullExtension: []
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface remoteObj {
+	name: string;
+	path: string;
+}
+
+class DVC {
+	plug: Plugin;
+	cwd: () => string;
+	remote: remoteObj[];
+	files: TFile[];
+	statusBarItem: any;
+
+	constructor(plug: Plugin) {
+		this.plug = plug;
+		this.cwd = () => {
+			if (this.plug.app.vault.adapter instanceof FileSystemAdapter) {
+				return this.plug.app.vault.adapter.getBasePath();
+			}
+			return '';
+		};
+
+		this.getFiles();
+	}
+
+	shell(command: string, show: boolean = true): Promise<string> {
+		return new Promise((resolve, reject) => {
+			exec(command, {cwd: this.cwd()}, (err, stdout, stderr) => {
+				if (err) {
+					console.log(err);
+					new Notice(stderr);
+					reject(stderr);
+					return;
+				}
+				resolve(stdout);
+				if (show) {
+					console.log(stdout)
+					new Notice(stdout);
+				}
+			})
+		});
+	}
+
+	cli(command: string, argument: any, show: boolean = true): void {
+		let arg: string = '';
+		if (typeof argument == 'string') {
+			arg = argument;
+		} else {
+			if (Array.isArray(argument)) {
+				arg = argument.map(file => `"${file.path}"`).join(" ");
+			} else {
+				arg = `"${argument.path}"`;
+			}
+		}
+		this.shell(`dvc ${command} ${arg}`, show);
+	}
+
+	status(show: boolean = true): void {
+		this.cli('status', "", show);
+	}
+
+	add(arg: any, show: boolean = true): void {
+		this.cli('add', arg);
+		this.getFiles();
+	}
+
+	push(arg: any, show: boolean = true): void {
+		this.cli('push', arg);
+	}
+
+	pull(arg: any, show: boolean = true): void {
+		this.cli('pull', arg);
+	}
+
+	getFiles(): void {
+		this.files = this.plug.app.vault.getFiles().filter(file => file.extension == 'dvc');
+	}
+
+	getRemote(): Promise<remoteObj[]> {
+		return new Promise((resolve, reject) => {
+			this.shell('dvc remote list')
+				.then((data) => {
+					const lines: string[] = data.split(/\r?\n/);
+					this.remote = lines.map((value) => {
+						const substr: string[] = value.split('\t');
+						return {name: substr[0], path: substr[1]};
+					});
+					resolve(this.remote);
+				})
+				.catch((err) => reject(err));
+		});
+	}
+}
+
+export default class DVCPlugin extends Plugin {
+	settings: DVCPluginSettings;
+	dvc: DVC;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.dvc = new DVC(this);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// adds git/dvc initalization
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'dvc-init',
+			name: 'dvc: init',
 			callback: () => {
-				new SampleModal(this.app).open();
+				this.dvc.shell('git init && dvc init -f');
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
+		// todo create/select dvc remote
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+			id: 'dvc-remote',
+			name: 'dvc: remote',
+			callback: () => {
+				this.dvc.getRemote().then((data) => {
+					console.log(data);
+				})
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+
+		// adds settings tab
+		this.addSettingTab(new DVCSettingTab(this.app, this));
+
+		// adds context menus for file
+		this.registerEvent(
+			this.app.workspace.on('file-menu', (menu, file) => {
+				[{com: "add", icon: "book-plus"}, {com: "push", icon: "book-up"},
+					{com: "pull", icon: "book-down"}]
+						.map(element => {
+							menu.addItem((item) => {
+								item
+								.setTitle(`dvc: ${element.com}`)
+								.setIcon(element.icon)
+								.onClick(async () => {
+									this.dvc.cli(element.com, [file]);
+								});
+						});
+				})
+			})
+		);
+
+		// adds context menus for files
+		this.registerEvent(
+			this.app.workspace.on('files-menu', (menu, files) => {
+				[{com: "add", icon: "book-plus"}, {com: "push", icon: "book-up"},
+					{com: "pull", icon: "book-down"}]
+						.map(element => {
+							menu.addItem((item) => {
+								item
+								.setTitle(`dvc: ${element.com}`)
+								.setIcon(element.icon)
+								.onClick(async () => {
+									this.dvc.cli(element.com, files);
+								});
+						});
+				})
+			})
+		);
+
+		// adds dvc auto pull attachment files
+		this.registerEvent(
+			this.app.workspace.on('file-open', async (file) => {
+				if (file && this.settings.autopull) {
+					const fileCache = this.app.metadataCache.getFileCache(file);
+					if (fileCache && fileCache.embeds) {
+						const dvcFiles = fileCache.embeds.map(embed => {
+							if (this.settings.autopullExtension.some(item => embed.link.includes(item))) {
+								return this.dvc.files.find(dvcFile => dvcFile.basename == embed.link);
+							}
+							return null;
+						})
+						if (dvcFiles) {
+							this.dvc.pull(dvcFiles.filter(item => item));
+						}
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
-			}
-		});
+			})
+		);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.dvc.status();
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -89,28 +211,13 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class DVCSettingTab extends PluginSettingTab {
+	plugin: DVCPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DVCPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +228,36 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Auto stage')
+			.setDesc('Enable git stage files after dvc adding')
+			.addToggle(component => component
+				.setValue(this.plugin.settings.autostage)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.dvc.cli('config', `--local core.autostage ${value}`, false);
+					this.plugin.settings.autostage = value;
+					await this.plugin.saveSettings();
+				}))
+
+		new Setting(containerEl)
+			.setName('Auto data pull')
+			.setDesc('Enable file attachment pull')
+			.addToggle(component => component
+				.setValue(this.plugin.settings.autopull)
+				.onChange(async (value) => {
+					this.plugin.settings.autopull = value;
+					await this.plugin.saveSettings();
+				}))
+	
+		new Setting(containerEl)
+			.setName('Extension list of auto pull mode')
+			.setDesc('File attachment extension list to auto pull')
+			.addText(text => text
+				.setPlaceholder('Enter a list of extensions separated by spaces')
+				.setValue(this.plugin.settings.autopullExtension.join(" "))
+				.onChange(async (value) => {
+					this.plugin.settings.autopullExtension = value.trim().replace(/\s+/g, " ").split(" ");
 					await this.plugin.saveSettings();
 				}));
+
 	}
 }
